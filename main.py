55bf9da6feaed32f19366f9d8b4a136ecae0aeda
MrGawron <55848064+MrGawron@users.noreply.github.com>
2022-05-25 17:27:58 +0200

    first try

--- main.py
+++ main.py
@@ -1,3 +1,22 @@
+import csv
+
+def main():
+    numbers = []
+    with open("phoneCalls.csv") as file:
+        csv_reader = csv.reader(file, delimiter = ",")
+        line_count = 0
+        for row in csv_reader:
+            if line_count == 0:
+                line_count += 1
+            else:
+                numbers.append(row[0])
+    numbers_set = set(numbers)
+    result = []
+    for number in numbers_set:
+        result.append((number, numbers.count(number)))
+    result = sorted(result, key = lambda x: -x[1])
+    max = result[0]
+    print(max)
 
 if __name__ == "__main__":
-    pass
+    main()
